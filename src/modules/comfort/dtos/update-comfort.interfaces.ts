f1e7d2312b673b06b86c140eaac14c9ae0275527
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2024-05-13 14:11:26 +0500

    update edit comfort name

--- src/modules/comfort/dtos/update-comfort.interfaces.ts
+++ src/modules/comfort/dtos/update-comfort.interfaces.ts
@@ -1,14 +1,8 @@
-import { IsOptional, IsUUID } from 'class-validator';
+import { IsUUID } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
 
 export class UpdateComfortDto {
-  @Transform((val) => {
-    if (!val) return undefined;
-    else return val
-  })
   @ApiProperty()
-  @IsOptional()
   @IsUUID(4)
-  name?: string;
+  name: string;
 }
