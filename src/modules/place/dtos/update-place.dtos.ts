fc20d10b9270f6ff9887c7bc9c5dc922776f166e
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2024-05-13 12:46:24 +0500

    update editing place

--- src/modules/place/dtos/update-place.dtos.ts
+++ src/modules/place/dtos/update-place.dtos.ts
@@ -1,15 +1,9 @@
-import { IsOptional, IsUUID } from 'class-validator';
+import { IsUUID } from 'class-validator';
 
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
 
 export class UpdatePlaceDto {
-  @Transform((val) => {
-    if (!val) return undefined;
-    else return val
-  })
   @ApiProperty()
   @IsUUID(4)
-  @IsOptional()
-  name?: string;
+  name: string;
 }
