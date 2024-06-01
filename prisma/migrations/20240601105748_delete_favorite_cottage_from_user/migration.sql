c1f085951d0ea3fa6b28c2be8fc9af73a934e7a1
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2024-06-01 16:47:06 +0500

    add migrations

---
+++ prisma/migrations/20240601105748_delete_favorite_cottage_from_user/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `favorite_cottages` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "user" DROP COLUMN "favorite_cottages";
