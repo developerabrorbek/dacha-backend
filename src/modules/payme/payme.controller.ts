0859161582fa1c4f7f0ee47c9a82d9cbc818ee5f
developerabrorbek <abdulkhamidovabrorbek@gmail.com>
2024-12-23 20:43:13 +0500

    get statement update

--- src/modules/payme/payme.controller.ts
+++ src/modules/payme/payme.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, Body, Req, UseGuards, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Req,
+  UseGuards,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { PaymeService } from './payme.service';
 import { CheckoutPaymentDto, CreatePaymentDto } from './dto';
 import { PaymeError, PaymeMethod } from '@enums';
@@ -6,58 +14,62 @@ import { TransactionException } from '@exceptions';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { PaymeGuard } from '@guard';
 import { ConfigService } from '@nestjs/config';
-import { encode } from "base-64"
+import { encode } from 'base-64';
 import { CheckAuth } from '@decorators';
 
-@ApiTags("Payme")
+@ApiTags('Payme')
 @Controller('payme')
 export class PaymeController {
-  constructor(private readonly paymeService: PaymeService, private configService: ConfigService) {}
+  constructor(
+    private readonly paymeService: PaymeService,
+    private configService: ConfigService,
+  ) {}
 
   @HttpCode(HttpStatus.OK)
   @UseGuards(PaymeGuard)
-  @Post("pay")
+  @Post('pay')
   async pay(@Body() payload: CreatePaymentDto) {
     const { id, method, params } = payload;
 
-    switch(method) {
+    switch (method) {
       case PaymeMethod.CheckPerformTransaction: {
         await this.paymeService.checkPerformTransaction(params, id);
-        return {result: {allow: true}}
+        return { result: { allow: true } };
       }
       case PaymeMethod.CheckTransaction: {
-        const result =  await this.paymeService.checkTransaction(params, id);
-        return {result}
+        const result = await this.paymeService.checkTransaction(params, id);
+        return { result };
       }
       case PaymeMethod.CreateTransaction: {
-        const result =  await this.paymeService.createTransaction(params, id);
-        return {result}
+        const result = await this.paymeService.createTransaction(params, id);
+        return { result };
       }
       case PaymeMethod.PerformTransaction: {
-        const result =  await this.paymeService.performTransaction(params, id);
-        return {result}
+        const result = await this.paymeService.performTransaction(params, id);
+        return { result };
       }
       case PaymeMethod.CancelTransaction: {
-        const result =  await this.paymeService.cancelTransaction(params, id);
-        return {result}
+        const result = await this.paymeService.cancelTransaction(params, id);
+        return { result };
       }
       case PaymeMethod.GetStatement: {
-        const result =  await this.paymeService.getStatement(params);
-        return {result};
+        const result = await this.paymeService.getStatement(params);
+        return { result: { transactions: result } };
       }
       default: {
-        throw new TransactionException(PaymeError.MethodNotFound, id)
+        throw new TransactionException(PaymeError.MethodNotFound, id);
       }
     }
   }
 
   @ApiBearerAuth()
   @CheckAuth(true)
-  @Post("checkout")
+  @Post('checkout')
   async checkout(@Body() payload: CheckoutPaymentDto, @Req() request: any) {
     const currentUserId = request.userId;
 
-    const MERCHANT_ID = this.configService.getOrThrow<string>("payme.merchantId");
+    const MERCHANT_ID =
+      this.configService.getOrThrow<string>('payme.merchantId');
 
     const user = await this.paymeService.checkUser(currentUserId);
     const order = await this.paymeService.checkOrder(payload.orderId);
@@ -65,7 +77,7 @@ export class PaymeController {
     const amount = order.tariff.price * 100;
 
     const r = encode(
-      `m=${MERCHANT_ID};ac.user_id=${user.id};ac.product_id=${order.id};a=${amount};c=${payload.url}`
+      `m=${MERCHANT_ID};ac.user_id=${user.id};ac.product_id=${order.id};a=${amount};c=${payload.url}`,
     );
     return { url: `https://checkout.paycom.uz/${r}` };
   }
